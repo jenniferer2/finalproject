c38692e21c01ed36fd06e6d548c774d30cb5c5de
BT_1E10_11 <sample@bths.edu>
2022-05-12 12:29:45 -0400

    get it to work for one user first

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -7,17 +8,77 @@ import java.io.FileNotFoundException;
 public class Runner {
 
     public static void main (String args[]) {
-        System.out.println("Welcome to Journaling!");
+        try {
+            File f = new File("src/User.data");
+            Scanner s = new Scanner(f);
+            int line = 1;
+            String user = "";
+            String password = "";
+            Journal j = null;
+            // reading from the file line by line
+            while (s.hasNextLine()) {
+                String data = s.nextLine();
+                if (line == 1) {
+                    user = data.substring(data.indexOf(" ") + 1, data.indexOf(","));
+                    password = data.substring(data.indexOf(",") + 1);
+                }
+                if (line == 2) {
+                    String date = data.substring(data.indexOf(" ") + 1, data.indexOf(","));
+                    int rate = Integer.parseInt(data.substring(data.indexOf(",") + 1, data.indexOf("|")));
+                    String entry = data.substring(data.indexOf("|") + 1);
+                    j = new Journal(date, entry, rate);
+                }
+                line++;
+            }
+            s.close();
+            User u = new User(user, password, j);
+
+            System.out.println("Welcome to Journaling!");
+            System.out.println("1. Login" + "\n" + "2. Register");
+            Scanner in = new Scanner (System.in);
+            System.out.print("Enter your option: ");
+            String option = s.nextLine();
+            if (option.equals("1")) {
+                System.out.print("Enter username: ");
+                String userIn = s.nextLine();
+                if (userIn.equals(u.getUserName())) {
+                    System.out.print("Enter Password: ");
+                    String pass = s.nextLine();
+                    if (pass.equals(u.getPassword())) {
+                        System.out.println("Let's start journaling!");
+                    }
+                    else {
+                        System.out.println("This password is not correct");
+                    }
+            }
+                else {
+                    System.out.println("This username does not exist");
+                }
+        }
+            if (option.equals("2")) {
+
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+      /*  System.out.println("Welcome to Journaling!");
         Scanner in = new Scanner (System.in);
         System.out.println("1. Login" + "\n" + "2. Register");
         String answer = in.nextLine();
-        if (answer.equals("2")){
+
+       */
 
 
-        }
-        if (answer.equals("1")) {
-
-        }
 
 
     }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 
 public class User {
     private String userName;
@@ -20,5 +23,33 @@ public class User {
         return password;
     }
 
-    
-}
+    public ArrayList<Journal> getEntries () {
+        return entries;
+    }
+
+    public void setUser (String x) {
+        userName = x;
+    }
+
+    public void setPassword (String y) {
+        password = y;
+    }
+
+    public void save () {
+        try {
+            File f = new File("src/user.data");
+            f.createNewFile(); // this method will create the file if it does not exist, if it does exist, it does nothing
+            FileWriter fw = new FileWriter("src/user.data");
+            fw.write("USER: " + userName + ", " + password);
+            for (Journal x : entries) {
+            fw.write ("ENTRY: " + x.getDate() + ", " + x.getRate() + " |" + x.getEntry());
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Unable to create file");
+            e.printStackTrace();
+        }
+    }
+
+    }
+
