8fea46c06d438166b99bfec26140f1f02b885d2c
BT_1E10_11 <sample@bths.edu>
2022-05-12 11:29:26 -0400

    yes

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Runner {
+
+    public static void main (String args[]) {
+        System.out.println("Welcome to Journaling!");
+        Scanner in = new Scanner (System.in);
+        System.out.println("1. Login" + "\n" + "2. Register");
+        String answer = in.nextLine();
+        if (answer.equals("2")){
+
+
+        }
+        if (answer.equals("1")) {
+
+        }
+
+
+    }
 
 
 }
